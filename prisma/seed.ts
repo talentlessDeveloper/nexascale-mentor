e6cd8cd43ff21a4e7360bd8fab38e34ee94f0bce
talentlessDeveloper <kareemope52@gmail.com>
2023-12-02 16:37:45 +0100

    schema seed and model updates

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { tasks, userTasks, users } from "./data";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.userTask.deleteMany({});
+  await prisma.task.deleteMany({});
+  await prisma.user.deleteMany({});
+
+  await prisma.user.createMany({ data: users });
+
+  await prisma.task.createMany({
+    data: tasks,
+  });
+
+  await prisma.userTask.createMany({
+    data: userTasks,
+  });
+  console.log("Database connected successfully 🙌");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
