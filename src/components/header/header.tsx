a54797411f6635a0978908d292f8b99548d855b1
talentlessDeveloper <kareemope52@gmail.com>
2023-12-02 16:47:12 +0100

    header menu added

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import ThemeToggle from "../themes/toggle-theme";
 import {
-  ChevronUp,
   ChevronDown,
-  X,
-  Menu,
-  Home,
-  User,
-  LogOut,
-  Globe,
-  Code,
-  TabletSmartphone,
+  ChevronUp,
   Github,
+  Home,
+  LogOut,
+  Menu,
+  User,
+  X,
 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import ThemeToggle from "../themes/toggle-theme";
 import { Button } from "../ui/button";
+import HeaderMenu from "./header-menu";
 
 const Header = () => {
   const isLoggedin = true;
@@ -35,7 +33,7 @@ const Header = () => {
     right-0 bg-primary-foreground font-inter "
     >
       {/* Desktop vew */}
-      <nav className="flex items-center justify-between py-5 container  max-lg:px-6">
+      <nav className="container flex items-center justify-between py-5  max-lg:px-6">
         <Link href="/">
           <Image
             src="/images/nexascale-logo.png"
@@ -49,9 +47,7 @@ const Header = () => {
           <div className=" flex items-center justify-center gap-5 text-base font-semibold max-lg:hidden">
             <ul className=" flex items-center justify-center gap-5 uppercase italic  max-lg:hidden">
               <li className="relative after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:scale-0 after:bg-accent after:transition-transform after:duration-500 hover:after:scale-100">
-                <Link href="/challenges" >
-                  Challenges
-                </Link>
+                <Link href="/challenges">Challenges</Link>
               </li>
               <li className="relative after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:scale-0 after:bg-accent after:transition-transform after:duration-500 hover:after:scale-100">
                 <Link href="/solutions">Solutions</Link>
@@ -62,8 +58,14 @@ const Header = () => {
             </ul>
             {isLoggedin ? (
               <>
-                <div >
-                  <Image src="https://plus.unsplash.com/premium_photo-1675626492183-865d6d8e2e8a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHVzZXJ8ZW58MHx8MHx8fDA%3D" width={48} height={48} alt="avatar" className="object-cover rounded-full w-12 h-12" />
+                <div>
+                  <Image
+                    src="https://plus.unsplash.com/premium_photo-1675626492183-865d6d8e2e8a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
+                    width={48}
+                    height={48}
+                    alt="avatar"
+                    className="h-12 w-12 rounded-full object-cover"
+                  />
                 </div>
                 {dropdown && (
                   <ul className="absolute right-24 top-24 flex  flex-col  divide-y-2  rounded-lg bg-primary-foreground text-xs uppercase italic shadow-lg">
@@ -111,66 +113,7 @@ const Header = () => {
         </div>
       </nav>
       {/* Mobile Navigation */}
-      {mobile && (
-        <div className="absolute left-0 flex w-full flex-col uppercase italic 
-         gap-10 bg-primary-foreground divide-y divide-accent p-6 text-xl lg:hidden
-         ">
-          <Link href="/challenges" className="flex items-center gap-4">
-            <span>
-              <TabletSmartphone size={30} />
-            </span>
-            Challenges
-          </Link>
-          <Link href="/solutions" className="flex items-center gap-4 pt-4">
-            <span>
-              <Code size={30} />
-            </span>
-            Solutions
-          </Link>
-          <Link href="/resources" className="flex items-center gap-4 pt-4">
-            <span>
-              <Globe size={30} />
-            </span>
-            Resources
-          </Link>
-          {isLoggedin ? (
-            <>
-              <div className="pt-4">
-                  <Image src="https://plus.unsplash.com/premium_photo-1675626492183-865d6d8e2e8a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHVzZXJ8ZW58MHx8MHx8fDA%3D" width={48} height={48} alt="avatar" className="object-cover rounded-full w-12 h-12" />
-                </div>
-              <div className="ml-10 flex flex-col gap-6 divide-y divide-accent">
-                <Link href={"/dashboard"} className="flex items-center gap-4 pt-4">
-                  <span>
-                    <Home size={30} />
-                  </span>
-                  Dashboard
-                </Link>
-                <Link href={"/profile"} className="flex items-center gap-4 pt-4">
-                  <span>
-                    <User size={30} />
-                  </span>
-                  Profile
-                </Link>
-                <Link href="" className="flex items-center gap-4 pt-4">
-                  <span>
-                    <LogOut className="text-red-500" size={30} />
-                  </span>
-                  Logout
-                </Link>
-              </div>
-            </>
-          ) : (
-            <Button className="flex gap-2  rounded-full bg-accent/90 p-8 text-accent-foreground hover:bg-accent">
-              <span className="font-semibold uppercase italic">
-                Log in with Github
-              </span>{" "}
-              <span>
-                <Github />
-              </span>
-            </Button>
-          )}
-        </div>
-      )}
+      {mobile && <HeaderMenu isLoggedIn={isLoggedin} />}
     </header>
   );
 };
