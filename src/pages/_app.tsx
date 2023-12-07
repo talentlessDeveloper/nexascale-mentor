b84c6e31d32fbc2dd71dd27e045e074a7399a7ed
talentlessDeveloper <kareemope52@gmail.com>
2023-12-07 15:55:40 +0100

    toaster for toast notifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Layout from "~/components/layout";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,6 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Toaster position="bottom-center" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
