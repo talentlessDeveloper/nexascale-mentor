d2fad2af54d3091ed96d8ea27b5f6c5c534e044c
talentlessDeveloper <kareemope52@gmail.com>
2023-12-24 08:58:05 +0100

    remove Image and use inly CldImage

--- src/components/tasks/task-card.tsx
+++ src/components/tasks/task-card.tsx
@@ -1,6 +1,5 @@
 import { type Task } from "@prisma/client";
 import { CldImage } from "next-cloudinary";
-import Image from "next/image";
 import Link from "next/link";
 
 type TaskProps = {
@@ -10,25 +9,18 @@ type TaskProps = {
 const TaskCard = ({ task }: TaskProps) => {
   return (
     <div className="overflow-hidden rounded-tl-lg rounded-tr-lg shadow-lg shadow-black/10 dark:shadow-white/10">
-      <div className="relative h-80 lg:h-96 ">
-        <Link href={`/challenges/${task.id}`}>
-          {task.image.includes("cloudinary") ? (
-            <CldImage
-              src={task.image}
-              alt={task.title}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="h-full w-full rounded-none rounded-tl-lg rounded-tr-lg object-cover transition-transform duration-300 hover:scale-105"
-            />
-          ) : (
-            <Image
-              fill
-              src={task.image}
-              alt={task.title}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="h-full w-full rounded-none rounded-tl-lg rounded-tr-lg object-cover transition-transform duration-300 hover:scale-105"
-            />
-          )}
+      <div>
+        <Link
+          href={`/challenges/${task.id}`}
+          className="relative h-80 lg:h-96 "
+        >
+          <CldImage
+            src={task.image}
+            alt={task.title}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="h-full w-full rounded-none rounded-tl-lg rounded-tr-lg object-cover transition-transform duration-300 hover:scale-105"
+          />
         </Link>
       </div>
       <div className="flex flex-col gap-5 p-6">
