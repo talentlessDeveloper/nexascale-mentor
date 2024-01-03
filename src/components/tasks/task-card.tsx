a161df910b2c229182a1d30f6b494bb47d832219
talentlessDeveloper <kareemope52@gmail.com>
2024-01-03 11:13:58 +0100

    add edit and delete buttons for task only if you are admin

--- src/components/tasks/task-card.tsx
+++ src/components/tasks/task-card.tsx
@@ -1,4 +1,6 @@
 import { type Task } from "@prisma/client";
+import { Delete, Edit } from "lucide-react";
+import { useSession } from "next-auth/react";
 import { CldImage } from "next-cloudinary";
 import Link from "next/link";
 
@@ -7,6 +9,7 @@ type TaskProps = {
 };
 
 const TaskCard = ({ task }: TaskProps) => {
+  const { data: sessionData } = useSession();
   return (
     <div className="overflow-hidden rounded-tl-lg rounded-tr-lg shadow-lg shadow-black/10 dark:shadow-white/10">
       <div className="h-80 lg:h-96">
@@ -24,9 +27,21 @@ const TaskCard = ({ task }: TaskProps) => {
         </Link>
       </div>
       <div className="flex flex-col gap-5 p-6">
-        <Link href={`/challenges/${task.id}`}>
-          <h2 className="text-2xl font-semibold text-accent">{task.title}</h2>
-        </Link>
+        <div className="flex items-center justify-between gap-2">
+          <Link href={`/challenges/${task.id}`}>
+            <h2 className="text-2xl font-semibold text-accent">{task.title}</h2>
+          </Link>
+          {sessionData?.user.role === "admin" ? (
+            <div className="flex items-center gap-1 ">
+              <button>
+                <Delete className="text-red-500" />
+              </button>
+              <button>
+                <Edit className="text-emerald-500" />
+              </button>
+            </div>
+          ) : null}
+        </div>
         <ul className="flex gap-1 font-bold ">
           <li>Html</li>
           <li>CSS</li>
