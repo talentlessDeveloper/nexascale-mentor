ca48199175b79496b302d4bcac88af964a6eaf8c
talentlessDeveloper <kareemope52@gmail.com>
2024-01-29 15:22:31 +0100

    home page and 404 page creation

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <p>
+        <span className="text-xl font-semibold">404</span> | This page could not
+        be found.
+      </p>
+    </div>
+  );
+};
+
+export default Custom404;
